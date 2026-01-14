f3919879b08657ce9967f3f85fee599faeb6b122
AyushBasak001 <ayushbasak31@gmail.com>
2026-01-14 18:30:18 +0530

    Links  Setup

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,15 +2,32 @@ import { db } from '../utils/db.js';
 
 export const renderLoginPage = async (req, res) => {
   try {
-    const result = await db.query(
-      "SELECT * FROM users"
-    );
-    return res.status(200).render("login.ejs", {
-      userList: result.rows
-    });
+    return res.status(200).render("login.ejs");
 
   } catch (err) {
-    console.error("GET /enquiry error:", err.message);
+    console.error("GET /login error:", err.message);
+
+    return res.status(500).send("Internal server error");
+  }
+}
+
+export const loginUser = async (req, res) => {
+  try {
+    return res.status(200).render("user.ejs");
+
+  } catch (err) {
+    console.error("GET /login error:", err.message);
+
+    return res.status(500).send("Internal server error");
+  }
+}
+
+export const signupUser = async (req, res) => {
+  try {
+    return res.status(200).render("user.ejs");
+
+  } catch (err) {
+    console.error("GET /login error:", err.message);
 
     return res.status(500).send("Internal server error");
   }
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import {renderLoginPage} from '../controllers/loginController.js';
+import {
+    renderLoginPage,
+    loginUser,
+    signupUser
+} from '../controllers/loginController.js';
 
 const router = express.Router();
 
 router.get('/', renderLoginPage);
+router.post('/', loginUser);
+router.post('/signup', signupUser);
 
 export default router;
